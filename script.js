a204e05bd8ae28233d8707c7517be68475325a43
kianbuechner <83275558+kianbuechner@users.noreply.github.com>
2024-06-12 16:57:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.body;
+
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     body.classList.add('dark-mode');
+//     toggleSwitch.checked = true;
+// }
